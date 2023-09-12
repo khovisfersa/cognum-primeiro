b31bfcbf955a5bcb0cfab3aae71a0410b332bc67
khovisfersa <lucaskfsantos@gmail.com>
2023-09-12 02:56:31 -0300

    hello route

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.listen(3333);
+app.listen(3000);
 
-app.get('/', (req,res, next)=> {
-    res.status(200).send("Me mama")
+app.get('/hello', (req,res, next)=> {
+    res.status(200).send({"message":"Hello Cognum!"});
 })
